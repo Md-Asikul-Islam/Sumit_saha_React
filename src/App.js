e9b79a13f103eb12a93d63e32a62c8f03e6feb95
Md-Asikul-Islam <mdasikuli48@gmail.com>
2023-03-13 21:18:14 +0600

    commi is completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Clock from './lesson-6/From_Handling/Clock';
 function App() {
   return (
     <div className="App">
-      
+       <Clock/>
     </div>
   );
 }
 
 export default App;
+
