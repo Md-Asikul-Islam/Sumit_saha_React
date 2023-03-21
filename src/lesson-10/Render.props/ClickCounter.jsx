57bd6f981ba112108e8c83e464dc2640c3692104
Md-Asikul-Islam <mdasikuli48@gmail.com>
2023-03-21 15:58:59 +0600

    assik

---
+++ src/lesson-10/Render.props/ClickCounter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ClickCounter extends Component {
+  render() {
+    return (
+      <div>ClickCounter</div>
+    )
+  }
+}
---
+++ src/lesson-10/Render.props/HoverCounter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class HoverCounter extends Component {
+  render() {
+    return (
+      <div>HoverCounter</div>
+    )
+  }
+}
